81a4b3f0e84db52ca6084e317a3e8b1434a843ea
Natalia Rubio <nataliav.rubio@gmail.com>
2026-02-20 11:11:44 -0300

    refactor: remove veterinarian routes

--- apps/backend/src/routes/index.js
+++ apps/backend/src/routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const authRoutes = require('./auth');
 const healthRoutes = require('./health');
-const veterinarianRoutes = require('./veterinarian');
 const patientsRoutes = require('./patients');
 const responsibleRoutes = require('./responsibles');
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/health', healthRoutes);
-router.use('/veterinarian', veterinarianRoutes);
 router.use('/patients', patientsRoutes);
 router.use('/responsibles', responsibleRoutes);
 
