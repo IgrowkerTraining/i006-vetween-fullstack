ca85042dbec39dec2c8290a2362caa1a729c2838
Natalia Rubio <nataliav.rubio@gmail.com>
2026-03-12 20:59:09 -0300

    docs: agrego documentación Swagger y README del backend

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -4,8 +4,14 @@ const apiRoutes = require("./src/routes");
 const config = require("./src/config");
 const aiRoutes = require("./src/routes/AIReport");
 require("dotenv").config();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
 const app = express();
+
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 setupMiddleware(app);
 
